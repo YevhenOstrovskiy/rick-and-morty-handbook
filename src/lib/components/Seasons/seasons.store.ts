428ae43e9385994381f98cd1f78134aef6499928
Yevhen <steplerjeka@gmail.com>
2025-01-27 18:31:30 +0200

    bug cloudflare top level

--- src/lib/components/Seasons/seasons.store.ts
+++ src/lib/components/Seasons/seasons.store.ts
@@ -1,8 +1,9 @@
 import { writable } from 'svelte/store';
 import type Episode from '~/lib/types/episode';
-import { getAllEpisodes } from '~/lib/api/episodes/getAllEpisodes';
+import { getThisEpisodes } from '~/lib/api/episodes/getAllEpisodes';
 
 export const selectedSeasonId = writable<number>(1);
 export const episodes = writable<Episode[]>([]);
 
-await getAllEpisodes().then(episodes.set);
+const epis = await getThisEpisodes();
+episodes.set(epis);
